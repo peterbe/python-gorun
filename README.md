0f6853172147de7f20cf9e7a33bd525076c71f69
Peter Bengtsson <peter@fry-it.com>
2009-07-20 16:17:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+gorun
+=====
+
+(c) Peter Bengtsson, Fry-IT, peter@fry-it.com, 2009
+License: Python
+
+
+Using (py)inotify to run commands when files change
+---------------------------------------------------
+
+Tired of switching console, arrow-up, Enter, switch console back for
+every little change you make when you're writing code that has tests?
+Running with `gorun.py` enables you to just save in your editor and
+the tests are run automatically and immediately. 
+
+`gorun.py` does not use a slow pulling process which keeps taps on
+files modification time. Instead it uses the
+[inotify](http://en.wikipedia.org/wiki/Inotify) which is...
+
+> inotify is a Linux kernel subsystem that provides file system event
+> notification
+
+
+Installation
+------------
+
+This will only work on Linux which has the inotify module enabled in
+the kernel. (Most modern kernels do)
+
+First install [pyinotify](http://trac.dbzteam.org/pyinotify) the usual
+way:
+
+        $ easy_install pyinotify
+	
+Then download `gorun` and put `gorun.py` into your `~/bin` directory
+and make it executable. Then, create a settings file, which is just a
+python file that is expected to define a variable called
+`DIRECTORIES`. Here's an example:
+
+        DIRECTORIES = (
+	  ('some/place/', './myframework test --dir some/place'),
+	  ('some/place/unitests.py', 
+	   './myframework test --dir some/place --testclass Unittests'),
+	  ('/var/log/torrentsdownload.log',
+	   'growl downloads --logfile /var/log/torrentsdownload.log'),
+	)
+
+Save that file as, for example, `gorun_settings.py` and then start it
+like this:
+
+        $ gorun.py gorun_settings
+
+
+Disclaimer
+----------
+
+This code hasn't been extensively tested and relies on importing
+python modules so don't let untrusted morons fiddle with your dev
+environment.
+
+Todo
+----
+
+When doing Django development I often run on single test method over
+and over and over again till I get rid of all errors. When doing this
+I have to change the settings so it just runs one single test and when
+I'm done I go back to set it up so that it runs all tests when adjacent 
+code works. 
+
+This is a nuisance and I might try to solve that one day. If you have
+any tips please let me know. 
---
+++ gorun.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+#
+# Wrapper on pyinotify for running commands
+# (c) 2009 Peter Bengtsson, peter@fry-it.com
+# 
+# TODO: the lock file mechanism doesn't work! threading?
+#
+
+import os
+
+    
+# Prepare a lock file
+from tempfile import gettempdir
+LOCK_FILE = os.path.join(gettempdir(), 'gorunning.lock')
+
+from pyinotify import WatchManager, Notifier, ThreadedNotifier, ProcessEvent, EventsCodes
+
+wm = WatchManager()
+flags = EventsCodes.ALL_FLAGS
+#print flags.keys()
+#mask = FLAGS['IN_DELETE'] | FLAGS['IN_CREATE']  # watched events
+mask = flags['IN_MODIFY'] #| flags['IN_CREATE']
+
+def _find_command(path):
+    # path is a file
+    assert os.path.isfile(path)
+    # in dictionary lookup have keys as files and directories.
+    # if this path exists in there, it's a simple match
+    try:
+        return lookup[path]
+    except KeyError:
+        pass
+    # is the parent directory in there?
+    while path != '/':
+        path = os.path.dirname(path)
+        try:
+            return lookup[path]
+        except KeyError:
+            pass
+        
+def _ignore_file(path):
+    if path.endswith('.pyc'):
+        return True
+    if path.endswith('~'):
+        return True
+    if os.path.basename(path).startswith('.#'):
+        return True
+
+class PTmp(ProcessEvent):
+    def process_IN_CREATE(self, event):
+        if os.path.basename(event.pathname).startswith('.#'):
+            # backup file
+            return
+        print "Creating:", event.pathname
+        command = _find_command(event.pathname)
+
+    #def process_IN_DELETE(self, event):
+    #    print "Removing:", event.pathname
+    #    command = _find_command(event.pathname)
+
+    def process_IN_MODIFY(self, event):
+        if _ignore_file(event.pathname):
+            return
+            
+        if os.path.isfile(LOCK_FILE):
+            # command is still running
+            return
+        
+        print "Modifying:", event.pathname
+        command = _find_command(event.pathname)
+        if command:
+            open(LOCK_FILE, 'w').write("Running command\n\n%s\n" % command)
+            os.system(command)
+            if os.path.isfile(LOCK_FILE):
+                os.remove(LOCK_FILE)
+            print "Waiting for stuff to happen again..."
+            
+def start(actual_directories): 
+    p = PTmp()
+    notifier = Notifier(wm, p)
+    
+    for actual_directory in actual_directories:
+        print "ACTUAL_DIRECTORY", actual_directory
+        wdd = wm.add_watch(actual_directory, mask, rec=True)
+    
+    # notifier = Notifier(wm, p, timeout=10)
+    try:
+        print "Waiting for stuff to happen..."
+        notifier.loop()
+    except KeyboardInterrupt:
+        pass
+    
+    return 0
+
+lookup = {}
+
+def configure_more(directories):
+    actual_directories = set()
+
+    # Tune the configured directories a bit
+    for i, (path, cmd) in enumerate(directories):
+        if not path.startswith('/'):
+            path = os.path.join(os.path.abspath(os.path.dirname('.')), path)
+        if not (os.path.isfile(path) or os.path.isdir(path)):
+            raise OSError, "%s neither a file or a directory" % path
+        if os.path.isdir(path):
+            if path.endswith('/'):
+                # tidy things up
+                path = path[:-1]
+            actual_directories.add(path)
+        else:
+            # because we can't tell pyinotify to monitor files,
+            # when a file is configured, add it's directory
+            actual_directories.add(os.path.dirname(path)) 
+            
+        lookup[path] = cmd
+        
+    return actual_directories
+        
+
+if __name__=='__main__':
+    import sys
+    args = sys.argv[1:]
+    if not args:
+        print >>sys.stderr, "USAGE: %s importable_py_settings_file" %\
+          __file__
+        sys.exit(1)
+    
+    settings = args[-1]
+    if settings.endswith('.py'):
+        settings = settings[:-3]
+        
+    sys.path.append(os.path.abspath(os.curdir))
+    x = __import__(settings)
+    DIRECTORIES = x.DIRECTORIES
+    actual_directories = configure_more(DIRECTORIES)
+    
+    sys.exit(start(actual_directories))
