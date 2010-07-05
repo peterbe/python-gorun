99370c22b9dbd420cbd9be505158e34d7853afe0
Gautier Hayoun <ghayoun@gmail.com>
2010-07-05 19:24:38 +0800

    Added example setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+setup(name = 'python-gorun',
+        version = '0.2',
+        description = 'Wrapper on pyinotify for running commands (often tests)',
+        long_description = file(
+            os.path.join(os.path.dirname(__file__), 'README.md')
+        ).read(),
+        author='Peter Bengtsson',
+        author_email='peter@fry-it.com',
+        url = 'http://github.com/Peterbe/python-gorun',
+        classifiers = [
+             'Programming Language :: Python :: 2',
+             'Intended Audience :: Developers',
+             'Operating System :: POSIX :: Linux',
+             'Topic :: Software Development :: Testing'
+             'Topic :: Software Development :: Build Tools'
+       ],
+       scripts = ['gorun.py']
+)
