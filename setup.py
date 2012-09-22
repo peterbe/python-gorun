3b6fdf9749ce79963b611824274118f7292a6d0c
Mathieu Agopian <mathieu.agopian@gmail.com>
2012-09-22 23:12:02 +0200

    make gorun pip installable

--- setup.py
+++ setup.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python
 
 import os
+import setuptools  # for zip_safe and install_requires
 from distutils.core import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name = 'python-gorun',
-        version = '1.6',
-        description = 'Wrapper on pyinotify for running commands (often tests)',
-        long_description = read('README.md'),
-        author='Peter Bengtsson',
-        author_email='peter@fry-it.com',
-        url = 'http://github.com/peterbe/python-gorun',
-        classifiers = [
-             'Programming Language :: Python :: 2',
-             'Intended Audience :: Developers',
-             'Operating System :: POSIX :: Linux',
-             'Topic :: Software Development :: Testing'
-             'Topic :: Software Development :: Build Tools'
-       ],
-       scripts = ['gorun.py']
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+
+with open('requirements.txt') as reqs:
+    install_requires = [
+        line for line in reqs.read().split('\n') if (line and not
+                                                     line.startswith('--'))
+    ]
+
+
+setup(
+    name='gorun',
+    version='1.6',
+    description='Wrapper on pyinotify for running commands (often tests)',
+    long_description=long_description,
+    author='Peter Bengtsson',
+    author_email='peter@fry-it.com',
+    url='http://github.com/peterbe/python-gorun',
+    license='BSD',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    scripts=['gorun.py'],
+    zip_safe=False,
+    install_requires=install_requires,
 )
